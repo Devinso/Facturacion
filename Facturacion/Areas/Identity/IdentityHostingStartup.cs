85c501e3532da0d75a1460d60b526b86787a5b76
Roger Munguia <34775911+RogerMunguia@users.noreply.github.com>
2019-08-26 11:16:30 -0600

    login para usuario

---
+++ Facturacion/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Facturacion.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Facturacion.Areas.Identity.IdentityHostingStartup))]
+namespace Facturacion.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FacturacionIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FacturacionContext")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<FacturacionIdentityContext>();
+            });
+        }
+    }
+}
--- Facturacion/Models/FacturacionContext.cs
+++ Facturacion/Models/FacturacionContext.cs
@@ -26,7 +26,7 @@ namespace Facturacion.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+
                 optionsBuilder.UseSqlServer("Server=DESKTOP-V6Q5059;Database=Facturacion;Trusted_Connection=true;");
             }
         }
